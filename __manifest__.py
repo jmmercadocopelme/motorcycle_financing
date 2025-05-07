692eb2ccc43c9d9d9cf1a2f45dc788a4e74628d6
jmmercadocopelme <jmmercado@copelme.com>
2025-05-07 11:25:39 -0400

    Update __manifest__.py

--- __manifest__.py
+++ __manifest__.py
@@ -1,14 +1,13 @@
 {
-	​'name': 'Motorcycle Financing',
-	​'summary': 'Streamlines the loan application process for dealerships.',
-	​'description': '',
-	​'license': 'OPL-1',
-	​'category': 'Kawiil/Custom Modules',
-	​'author': 'jmmercadocopelme',
-	​'website': '',
-	​'version': '18.0.0.0.1',
-	​'depends': [],
-	​'data': [],
-	​'demo': [],
-	​'application': 'True',
-}
+    'name': 'Motorcycle Financing',
+    'summary': 'Streamlines the loan application process for dealerships.',
+    'license': 'OPL-1',
+    'category': 'Kawiil/Custom Modules',
+    'author': 'jmmercadocopelme',
+    'website': 'https://github.com/jmmercadocopelme/motorcycle_financing',
+    'version': '18.0.1.0.0',
+    'depends': [],
+    'data': [ ],
+    'demo': [
+    ],
+} 
