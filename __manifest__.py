16636a039401d122d10ed4be7263618591c688a8
jmmomercadocopelme <jmmercado@copelme.com>
2025-05-07 11:07:48 -0400

    new file:   __init__.py new file:   __manifest__.py new file:   static/description/icon.png

---
+++ __manifest__.py
@@ -0,0 +1,14 @@
+{
+	​'name': 'Motorcycle Financing',
+	​'summary': 'Streamlines the loan application process for dealerships.',
+	​'description': '',
+	​'license': 'OPL-1',
+	​'category': 'Kawiil/Custom Modules',
+	​'author': 'jmmercadocopelme',
+	​'website': '',
+	​'version': '18.0.0.0.1',
+	​'depends': [],
+	​'data': [],
+	​'demo': [],
+	​'application': 'True',
+}
