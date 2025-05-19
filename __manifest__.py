1b21e0bfba8f0a744e6573e897e85c94d53ea077
jmmercadocopelme <jmmercado@copelme.com>
2025-05-19 14:23:35 -0400

    Update __manifest__.py

--- __manifest__.py
+++ __manifest__.py
@@ -10,12 +10,16 @@
         'security/ir.model.access.csv',
         'security/rules.xml',
         'data/loan_demo.xml',
-        'views/motorcycle_financing_menu.xml',
+    ],
+    
+    'views': [
         'views/loan_application_views.xml',
         'views/loan_application_tag_views.xml',
         'views/loan_application_document_views.xml',
         'views/loan_application_document_type_views.xml',
+        'views/motorcycle_financing_menu.xml',
     ],
+
     'demo': [
         'data/loan_demo.xml',
     ],
